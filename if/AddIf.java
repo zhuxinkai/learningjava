bd2c4e3c4b891cae6b45d477a0b9e2af4af2aa6b
zhuxinkai <zhuxinkai@126.com>
2019-01-28 23:07:58 +0800

    if

---
+++ if/AddIf.java
@@ -0,0 +1,28 @@
+class AddIf{
+	public static void main (String [] args){
+		int time = 10;
+		if (time == 10){
+			System.out.println(time);
+		int source = 90;
+		if (source >= 80){
+			
+			
+			
+			System.out.println("恭喜你取得优异成绩");
+			
+			
+			
+		}	
+			
+		System.out.println("程序结束");
+		
+			
+			
+		}		
+		
+		
+		
+	}
+	
+	
+}
