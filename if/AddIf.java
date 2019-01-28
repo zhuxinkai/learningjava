58b9d7b99d773df93dfbc5a39e77f26618d95eb8
zhuxinkai <zhuxinkai@126.com>
2019-01-28 23:15:22 +0800

    Update AddIf.java

--- if/AddIf.java
+++ if/AddIf.java
@@ -3,16 +3,17 @@ class AddIf{
 		int time = 10;
 		if (time == 10){
 			System.out.println(time);
-		int source = 90;
-		if (source >= 80){
-			
+		int source = 40;
+		if (source >= 80)
 			
 			
 			System.out.println("恭喜你取得优异成绩");
+			System.out.println("恭喜你取得优异成绩1");
+			System.out.println("恭喜你取得优异成绩2");
 			
+			//如果if没有{}作为IF体，那么IF的直接下一行，作为IF体。
 			
-			
-		}	
+		
 			
 		System.out.println("程序结束");
 		
